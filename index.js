7bf494ce249c23444b060607c321bf77666cb2e9
CRSpradlin <github@crspradlin.org>
2021-10-01 12:48:00 -0400

    initial commit for Gabe

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const gpio = require('onoff').Gpio;
+const led = new gpio(23, 'out');
+
+const flipLed = async () => {
+  const currVal = await led.read();
+  await led.write(currVal ^ 1);
+  return (currVal ^ 1);
+}
+
+app.get('/flip', async (req, res) => {
+  const ledValue = await flipLed();
+  res.send('good flip<br>Current Value: ' + ledValue);
+});
+
+app.listen(8081);
